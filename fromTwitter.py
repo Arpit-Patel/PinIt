956a86f6b3598a4e5e412eede05d1148d4bddc95
jlsajfj <jlsajfj@hotmail.com>
2017-08-26 22:21:52 -0300

    slight fix

--- fromTwitter.py
+++ fromTwitter.py
@@ -1,6 +1,7 @@
 import requests, json, os
 
 def getTwitter(hashtag, count):
+  cnt=0
   tBlock=[]
   url='https://api.twitter.com/1.1/search/tweets.json?q=%23' + hashtag
   headers={'Authorization':'Bearer AAAAAAAAAAAAAAAAAAAAADtO2AAAAAAAfih8%2FpRhXRp5L6IPZYqNH0mn4Gg%3DKSoXjRQo2AgfYtKKwfgNyWkYEvfub6frbY9ykCJ5zkz0kjzxWm'}
@@ -10,8 +11,8 @@ def getTwitter(hashtag, count):
     fullurl = "https://twitter.com/"+a['user']['screen_name']+"/status/"+str(a['id'])
     full = requests.get(fullurl)
     tBlock.append(full.json()['html'].replace("//platform.twitter","https://platform.twitter"))
-    count+=1
-    if count == 5:
+    cnt+=1
+    if cnt >= 5:
       break
 
   return tBlock
